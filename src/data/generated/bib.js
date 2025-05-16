d0bf61a5cf303ce4f63b86172d338ac8d08d9b35
syaoranhuan <syaoranhuan@gmail.com>
2025-05-16 01:04:36 +0100

    Add files via upload

---
+++ src/data/generated/bib.js
@@ -0,0 +1,117 @@
+﻿define({ entries : {
+    "Cardinale2012": {
+        "author": "Cardinale, Bradley J. and Duffy, J. Emmett and Gonzalez, Andrew and Hooper, David U. and Perrings, Charles and Venail, Patrick and Narwani, Anita and Mace, Georgina M. and Tilman, David and Wardle, David A. and Kinzig, Ann P. and Daily, Gretchen C. and Loreau, Michel and Grace, James B. and Larigauderie, Anne and Srivastava, Diane S. and Naeem, Shahid",
+        "doi": "10.1038/nature11148",
+        "journal": "Nature",
+        "keywords": "biodiversity, ecosystem services, global change, extinction, sustainability",
+        "number": "7401",
+        "pages": "59--67",
+        "title": "Biodiversity loss and its impact on humanity",
+        "type": "article",
+        "volume": "486",
+        "year": "2012"
+    },
+    "Eldridge2018": {
+        "author": "Eldridge, Alice and Guyot, Patrice and Moscoso, Paola and Johnston, Alison and Eyre-Walker, Ying and Peck, Mika",
+        "doi": "10.1016/j.ecolind.2018.06.012",
+        "journal": "Ecological Indicators",
+        "keywords": "ecoacoustic indices, species richness, acoustic diversity, temperate vs tropical, bird monitoring",
+        "number": "1",
+        "pages": "939--952",
+        "title": "Sounding out ecoacoustic metrics: Avian species richness is predicted by acoustic indices in temperate but not tropical habitats",
+        "type": "article",
+        "volume": "95",
+        "year": "2018"
+    },
+    "Ibrahim2021": {
+        "author": "Ibrahim, Ali K. and Zhuang, Hanqi and Ch\u00e9rubin, Laurent M. and Erdol, Nurgun and O'Corry-Crowe, Gregory and Ali, Ali Muhamed",
+        "doi": "10.1121/10.0005898",
+        "journal": "The Journal of the Acoustical Society of America",
+        "keywords": "marine mammals, whale detection, deep learning, multimodel fusion, conservation acoustics",
+        "number": "2",
+        "pages": "1264--1272",
+        "title": "A multimodel deep learning algorithm to detect North Atlantic right whale up-calls",
+        "type": "article",
+        "volume": "150",
+        "year": "2021"
+    },
+    "Morgan2021": {
+        "author": "Morgan, M. M. and Braasch, J.",
+        "doi": "10.1016/j.ecoinf.2021.101242",
+        "journal": "Ecological Informatics",
+        "keywords": "long-term monitoring, deep learning, environmental sound, acoustic analysis, urban ecology",
+        "pages": "101242",
+        "title": "Long-term deep learning-facilitated environmental acoustic monitoring in the Capital Region of New York State",
+        "type": "article",
+        "volume": "61",
+        "year": "2021"
+    },
+    "NietoMora2023": {
+        "author": "Nieto-Mora, D.A. and Rodr\u00edguez-Buritica, Susana and Rodr\u00edguez-Mar\u00edn, Paula and Mart\u00ednez-Vargaz, J.D. and Isaza-Narv\u00e1ez, Claudia",
+        "doi": "10.1016/j.heliyon.2023.e20275",
+        "journal": "Heliyon",
+        "keywords": "ecoacoustics, machine learning, soundscape monitoring, systematic review, classification, clustering",
+        "number": "10",
+        "pages": "e20275",
+        "title": "Systematic review of machine learning methods applied to ecoacoustics and soundscape monitoring",
+        "type": "article",
+        "volume": "9",
+        "year": "2023"
+    },
+    "Ptacek2016": {
+        "author": "Ptacek, L. and Machlica, L. and Linhart, P. and Jaska, P. and Muller, L.",
+        "doi": "10.1080/09524622.2015.1089524",
+        "journal": "Bioacoustics",
+        "keywords": "individual recognition, bird calls, open-set classification, signal processing, wildlife monitoring",
+        "number": "1",
+        "pages": "55--73",
+        "title": "Automatic recognition of bird individuals on an open set using as-is recordings",
+        "type": "article",
+        "volume": "25",
+        "year": "2016"
+    },
+    "Quinn2022": {
+        "author": "Quinn, Colin A. and Burns, Patrick and Gill, Gurman and Baligar, Shrishail and Snyder, Rose L. and Salas, Leonardo and Goetz, Scott J. and Clark, Matthew L.",
+        "doi": "10.1016/j.ecolind.2022.108831",
+        "journal": "Ecological Indicators",
+        "keywords": "soundscape classification, CNN, ecoacoustic patterns, geographic variation, temporal analysis",
+        "pages": "108831",
+        "title": "Soundscape classification with convolutional neural networks reveals temporal and geographic patterns in ecoacoustic data",
+        "type": "article",
+        "volume": "138",
+        "year": "2022"
+    },
+    "Ruff2021": {
+        "author": "Ruff, Zachary J. and Lesmeister, Damon B. and Appel, Cara L. and Sullivan, Christopher M.",
+        "doi": "10.1016/j.ecolind.2021.107419",
+        "journal": "Ecological Indicators",
+        "keywords": "animal sound identification, convolutional neural networks, acoustic classification, workflow, automation",
+        "pages": "107419",
+        "title": "Workflow and convolutional neural network for automated identification of animal sounds",
+        "type": "article",
+        "volume": "124",
+        "year": "2021"
+    },
+    "Stowell2022": {
+        "author": "Stowell, Dan",
+        "doi": "10.7717/peerj.13152",
+        "journal": "PeerJ",
+        "keywords": "bioacoustics, deep learning, soundscape, review, ecoacoustics, CNN, data challenges",
+        "pages": "e13152",
+        "title": "Computational bioacoustics with deep learning: a review and roadmap",
+        "type": "article",
+        "volume": "10",
+        "year": "2022"
+    },
+    "Zhong2020": {
+        "author": "Zhong, Ming and LeBien, Jack and Campos-Cerqueira, Marconi and Dodhia, Rahul and Lavista Ferres, Juan and Velev, Julian P. and Aide, T. Mitchell",
+        "doi": "10.1016/j.apacoust.2020.107375",
+        "journal": "Applied Acoustics",
+        "keywords": "bioacoustic classification, transfer learning, pseudo-labeling, CNN, multispecies detection",
+        "pages": "107375",
+        "title": "Multispecies bioacoustic classification using transfer learning of deep convolutional neural networks with pseudo-labeling",
+        "type": "article",
+        "volume": "166",
+        "year": "2020"
+    }
+}});
